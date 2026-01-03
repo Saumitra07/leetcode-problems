44c3ae45c10e64d48045aa55e7b6228eed13a398
Saumitra Apte <saumitra.apte31@gmail.com>
2026-01-03 18:42:49 -0500

    old changes

---
+++ com/company/Graphs/PacificAtantic.java
@@ -0,0 +1,5 @@
+package com.company.Graphs;
+
+public class PacificAtantic {
+    
+}
---
+++ com/company/TaskScheduler.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class TaskScheduler {
+    public static void main(String[] args) {
+       char[] tasks = new char[]{'A','A','A','A','A','A','B','C','D','E','F','G'};
+       int n=2;
+       System.out.println(leastInterval(tasks,n));
+    }
+
+    public static int leastInterval(char[] tasks,int n)
+    {
+        Map<Character, Integer> counts = new HashMap<Character, Integer>();
+        for (char t : tasks) {
+            counts.put(t, counts.getOrDefault(t, 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> b - a);
+        pq.addAll(counts.values());
+
+        int alltime = 0;
+        int cycle = n + 1;
+        while (!pq.isEmpty()) {
+            int worktime = 0;
+            List<Integer> tmp = new ArrayList<Integer>();
+            for (int i = 0; i < cycle; i++) {
+                if (!pq.isEmpty()) {
+                    tmp.add(pq.poll());
+                    worktime++;
+                }
+            }
+            for (int cnt : tmp) {
+                if (--cnt > 0) {
+                    pq.offer(cnt);
+                }
+            }
+            alltime += !pq.isEmpty() ? cycle : worktime;
+        }
+        
+        return alltime;
+    }
+}
